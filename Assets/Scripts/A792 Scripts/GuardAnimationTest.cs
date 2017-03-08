ecc4bf66d2f17f2aa58b6e15902b2e6605d41d13
Trevor Vaughn <TrevorJVaughn@gmail.com>
2017-03-07 23:07:29 -1000

    Running, aiming, shooting, and throwing animations, test script to make sure the ragdolls don't freak out on death

---
+++ Assets/Scripts/A792 Scripts/GuardAnimationTest.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardAnimationTest : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody rb;
+    Rigidbody[] limbRBs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        limbRBs = GetComponentsInChildren<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isFiring", false);
+            anim.SetBool("isThrowing", false);
+            anim.SetBool("isAiming", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isFiring", false);
+            anim.SetBool("isThrowing", false);
+            anim.SetBool("isAiming", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isFiring", true);
+            anim.SetBool("isThrowing", false);
+            anim.SetBool("isAiming", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isFiring", false);
+            anim.SetBool("isThrowing", true);
+            anim.SetBool("isAiming", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            foreach(Rigidbody tempRig in limbRBs)
+            {
+                tempRig.velocity = Vector3.zero;
+            }
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = false;
+            Destroy(anim);
+        }
+    }
+}
