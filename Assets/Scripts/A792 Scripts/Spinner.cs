11dcc4ffe71b493dbb0970cb87973fbe8ef00300
Trevor Vaughn <TrevorJVaughn@gmail.com>
2017-03-23 22:01:30 -1000

    Most of "Officer Toots"

---
+++ Assets/Scripts/A792 Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+    public float spinSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // spin
+        gameObject.transform.Rotate(Vector3.up * (spinSpeed * Time.deltaTime));
+	}
+}
