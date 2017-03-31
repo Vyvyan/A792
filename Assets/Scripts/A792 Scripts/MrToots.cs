6f96013edbf654374a854c38076dba892269ef7c
Trevor Vaughn <TrevorJVaughn@gmail.com>
2017-03-30 20:18:22 -1000

    Mr. Toots basic ai, mr toots death

---
+++ Assets/Scripts/A792 Scripts/MrToots.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MrToots : MonoBehaviour {
+
+    // references to things we need to destroy on death/change
+    public s_WanderingAI wanderingScript;
+    public GameObject deadToots, aliveToots, siren, arm1, arm2;
+    public NavMeshAgent agent;
+
+    [Header("Health Stuff")]
+    public int health;
+
+    public bool isAlive;
+
+    // Use this for initialization
+    void Start ()
+    {
+        isAlive = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (isAlive)
+        {
+            if (health <= 0)
+            {
+                Kill();
+            }
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            TakeDamage(2, other.gameObject.transform.position);
+            other.gameObject.tag = "Untagged";
+        }
+    }
+
+    public void TakeDamage(int damage, Vector3 hitLocation)
+    {
+        if (isAlive)
+        {
+            Debug.Log("we took " + damage.ToString() + " damage.");
+            // remove health
+            health -= damage;
+
+            // if we have no health, kill us
+            if (health <= 0)
+            {
+                Kill();
+            }
+        }
+    }
+
+    void Kill()
+    {
+        Destroy(aliveToots);
+        Destroy(wanderingScript);
+        Destroy(agent);
+        deadToots.SetActive(true);
+        arm1.AddComponent<Rigidbody>();
+        arm2.AddComponent<Rigidbody>();
+        siren.AddComponent<Rigidbody>();
+        Destroy(gameObject.GetComponent<BoxCollider>());
+        AutoDestroy autoDestroy = gameObject.transform.parent.gameObject.AddComponent<AutoDestroy>();
+        autoDestroy.timeUntilDestroy = 8;
+        isAlive = false;
+    }
+}
---
+++ Assets/Scripts/A792 Scripts/MrTootsParts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MrTootsParts : MonoBehaviour {
+
+    public MrToots parentScript;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            if (other.relativeVelocity.magnitude > 5)
+            {
+                parentScript.TakeDamage(2, other.gameObject.transform.position);
+                other.gameObject.tag = "Untagged";
+            }
+        }
+    }
+}
